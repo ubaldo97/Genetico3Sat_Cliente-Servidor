46c9e61b532fa16bbeaa7b94850ed3854a22f048
ruben <ruben@DESKTOP-64C812N>
2018-05-22 17:25:23 -0500

    Clase para hacer la conexion TCP

---
+++ src/Herramientas/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Herramientas;
+import java.io.*;
+import java.nio.*;
+import java.net.*;
+/**
+ *
+ * @author ubaldo
+ */
+public class Conexion {
+    private final int puerto = 20011;
+    private String host ="localhost";
+    protected ServerSocket ss;
+    protected String mensaje;
+    protected Socket cs;
+    protected ObjectOutputStream salidaServidor;
+    protected ObjectOutputStream salidaCliente;
+
+    public Conexion(String a)throws IOException {
+        if(a.equalsIgnoreCase("servidor"))
+        {
+            ss = new ServerSocket(puerto);
+            cs = new Socket(); 
+        }
+        else
+        {
+            cs = new Socket(host, puerto); 
+        }
+    }
+    
+    
+    
+}
